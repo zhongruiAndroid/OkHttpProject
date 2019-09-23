fa9fcdbaa93548868d5253c88e4d6e33f584bacc
windows10 <2380253499@qq.com>
2019-09-23 18:23:09 +0800

    file

---
+++ TheOkHttp/src/main/java/com/github/theokhttp/NetworkUtils.java
@@ -0,0 +1,37 @@
+package com.github.theokhttp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+public class NetworkUtils {
+    private static Context context;
+    public static Context getContext() {
+        return context;
+    }
+    public static void init(Context cxt) {
+        context = cxt;
+    }
+    public static boolean isNetworkAvailable() {
+        return isNetworkAvailable(context);
+    }
+    @SuppressLint("MissingPermission")
+    public static boolean isNetworkAvailable(Context context) {
+        if(context==null){
+            throw new IllegalArgumentException("context is null,please call NetworkUtils.init(this) in application");
+        }
+        ConnectivityManager cm =(ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&activeNetwork.isConnectedOrConnecting();
+    }
+
+    public static boolean noNetwork() {
+        return noNetwork(context);
+    }
+    public static boolean noNetwork(Context context) {
+        return !isNetworkAvailable(context);
+    }
+}
+
