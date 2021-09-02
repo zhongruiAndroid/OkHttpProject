a0380396826ae737242c772ef2a60b6d642be71e
zhongrui <zhongrui@021.com>
2021-09-02 18:04:54 +0800

    file

--- app/src/main/java/com/test/okhttp/Main2Activity.java
+++ app/src/main/java/com/test/okhttp/Main2Activity.java
@@ -2,7 +2,7 @@ package com.test.okhttp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.github.theokhttp.TheOkHttp;
